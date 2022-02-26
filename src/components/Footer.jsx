abf3ae8ebc972da085bc10edb0af267b4286c327
Asif Rana <muhammadasif@MUHAMMADS-MBP.station>
2022-02-26 01:40:29 +0000

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="footer  bg-cover bg-top">
+        <footer className="footer  bg-cover bg-top" id="footer">
             <div className="wrapper text-center py-20 text-white">
                 <h2 className=" font-bold text-3xl">Join Today</h2>
                 <p className="md:w-1/2 mx-auto my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a corporis culpa, aperiam dolores corrupti sint ad ut incidunt quas in dolore aliquam sequi consequuntur perferendis debitis asperiores sed molestiae!</p>
